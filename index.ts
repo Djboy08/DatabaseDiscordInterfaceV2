8ff136dfb5e2d25e613a4003f1f05623ec8bd3ad
Djboy08 <justindoane@gmail.com>
2026-01-31 17:15:07 -0500

    test

--- index.ts
+++ index.ts
@@ -37,12 +37,6 @@ const client = new Client({
     GatewayIntentBits.GuildPresences,
   ],
 });
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
-// client.once(Events.ClientReady, (readyClient: ClientType<true>) => {
-//   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-// });
 
 client.commands = new Collection();
 client.cooldowns = new Collection();
@@ -237,3 +231,12 @@ const server = Bun.serve({
 console.log(`Server running at ${server.url}`);
 
 client.login(Bun.env.DISCORD_TOKEN);
+client.on(Events.ShardError, (error) => {
+  console.error("A websocket connection encountered an error:", error);
+});
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, (readyClient: ClientType<true>) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
