627fdeafd638f1fd3cb4f9191a9f97d7584d50e2
Djboy08 <justindoane@gmail.com>
2026-02-01 15:37:10 -0500

    fix

--- index.ts
+++ index.ts
@@ -168,7 +168,7 @@ const loggingServer = Bun.serve({
         }
       },
     },
-    "/trade/webhook/": {
+    "/trade/webhook/2": {
       POST: async (req: Request) => {
         try {
           let headers = req.headers;
