e81051e01822c6318e9efeba81504090ec1ed3b7
Djboy08 <justindoane@gmail.com>
2026-01-31 22:45:42 -0500

    f"

--- index.ts
+++ index.ts
@@ -41,192 +41,192 @@ const client = new Client({
 client.commands = new Collection();
 client.cooldowns = new Collection();
 client.modals = new Collection();
-// client.db = await mongoClient.db(Bun.env.DATABASE_NAME);
-// const foldersPath = path.join(__dirname, "commands");
-// const commandFolders = fs.readdirSync(foldersPath);
-// for (const folder of commandFolders) {
-//   const commandsPath = path.join(foldersPath, folder);
-//   const commandFiles = fs
-//     .readdirSync(commandsPath)
-//     .filter((file: any) => file.endsWith(".js") || file.endsWith(".ts"));
-//   for (const file of commandFiles) {
-//     const filePath = path.join(commandsPath, file);
-//     const command = require(filePath);
-//     if ("data" in command && "execute" in command) {
-//       client.commands.set(command.data.name, command);
-//     } else {
-//       console.log(
-//         `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
-//       );
-//     }
-//   }
-// }
+client.db = await mongoClient.db(Bun.env.DATABASE_NAME);
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file: any) => file.endsWith(".js") || file.endsWith(".ts"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
+      );
+    }
+  }
+}
 
-// const eventsPath = path.join(__dirname, "events");
-// const eventFiles = fs
-//   .readdirSync(eventsPath)
-//   .filter((file: any) => file.endsWith(".js") || file.endsWith(".ts"));
-// for (const file of eventFiles) {
-//   const filePath = path.join(eventsPath, file);
-//   const event = require(filePath);
-//   if (event.once) {
-//     client.once(event.name, (...args) => event.execute(...args));
-//   } else {
-//     client.on(event.name, (...args) => event.execute(...args));
-//   }
-// }
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file: any) => file.endsWith(".js") || file.endsWith(".ts"));
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
 
-// const modalsPath = path.join(__dirname, "modals");
-// const modalFiles = fs
-//   .readdirSync(modalsPath)
-//   .filter((file: any) => file.endsWith(".js") || file.endsWith(".ts"));
-// for (const file of modalFiles) {
-//   const filePath = path.join(modalsPath, file);
-//   const modal = require(filePath);
-//   if ("execute" in modal) {
-//     client.modals.set(
-//       modal.name,
-//       async (interaction: any, initalInteraction: any) => {
-//         if (!interaction.member.roles.cache.has(modal.roleNeeded)) {
-//           await interaction.reply({
-//             content: "You do not have permission to use this command.",
-//             flags: 1 << 6, // Ephemeral
-//           });
-//           return;
-//         }
-//         console.log("Executing modal:", modal.name);
-//         await modal.execute(interaction, initalInteraction);
-//       },
-//     );
-//   }
-// }
+const modalsPath = path.join(__dirname, "modals");
+const modalFiles = fs
+  .readdirSync(modalsPath)
+  .filter((file: any) => file.endsWith(".js") || file.endsWith(".ts"));
+for (const file of modalFiles) {
+  const filePath = path.join(modalsPath, file);
+  const modal = require(filePath);
+  if ("execute" in modal) {
+    client.modals.set(
+      modal.name,
+      async (interaction: any, initalInteraction: any) => {
+        if (!interaction.member.roles.cache.has(modal.roleNeeded)) {
+          await interaction.reply({
+            content: "You do not have permission to use this command.",
+            flags: 1 << 6, // Ephemeral
+          });
+          return;
+        }
+        console.log("Executing modal:", modal.name);
+        await modal.execute(interaction, initalInteraction);
+      },
+    );
+  }
+}
 
-// // Log in to Discord with your client's token
-// setInterval(() => {
-//   unbanLengthCheckDatabase(client.db);
-// }, 1000 * 60);
+// Log in to Discord with your client's token
+setInterval(() => {
+  unbanLengthCheckDatabase(client.db);
+}, 1000 * 60);
 
-// let BANS: any = null;
-// setInterval(async () => {
-//   BANS = await getBans(client.db);
-// }, 1000 * 8);
+let BANS: any = null;
+setInterval(async () => {
+  BANS = await getBans(client.db);
+}, 1000 * 8);
 
-// const GUID = Bun.env.RBLX_GUID!;
-// const server = Bun.serve({
-//   port: 3000,
-//   // `routes` requires Bun v1.2.3+
-//   routes: {
-//     // Static routes
-//     "/exploit/webhook/t": {
-//       POST: async (req: Request) => {
-//         try {
-//           let headers = req.headers;
-//           if (
-//             !headers.get("authorization") ||
-//             headers.get("authorization") != GUID
-//           ) {
-//             console.log("NO AUTHORIZATION");
-//             return Response.json({
-//               success: false,
-//               error: "Incorrect Authorization",
-//             });
-//           }
+const GUID = Bun.env.RBLX_GUID!;
+const server = Bun.serve({
+  port: 3000,
+  // `routes` requires Bun v1.2.3+
+  routes: {
+    // Static routes
+    "/exploit/webhook/t": {
+      POST: async (req: Request) => {
+        try {
+          let headers = req.headers;
+          if (
+            !headers.get("authorization") ||
+            headers.get("authorization") != GUID
+          ) {
+            console.log("NO AUTHORIZATION");
+            return Response.json({
+              success: false,
+              error: "Incorrect Authorization",
+            });
+          }
 
-//           const hook = new WebhookClient({
-//             url: Bun.env.DISCORD_EXPLOIT_LOG_WEBHOOK_URL!,
-//           });
-//           const body = await req.json();
-//           const msg = body.msg;
-//           // let randomadmin = await this.discord.getRandomAdmin();
-//           // randomadmin = randomadmin || "No Admin Is Online";
-//           // if(msg.search("Dealing damage faster than a usual rate.") >= 0) return;
-//           const embed = new EmbedBuilder()
-//             .setTitle("Exploit Logged")
-//             .setFooter({ text: "from game" })
-//             .setDescription(msg);
-//           // hook.send({content: `<@${randomadmin.user.id}>`, embeds: [embed]});
-//           await hook.send({
-//             content: `Temp removed admin ping`,
-//             embeds: [embed],
-//           });
-//           return Response.json({ success: true });
-//         } catch (er) {
-//           return Response.json({ success: false, error: er });
-//         }
-//       },
-//     },
-//     "/trade/webhook/t": {
-//       POST: async (req: Request) => {
-//         try {
-//           let headers = req.headers;
-//           if (
-//             !headers.get("authorization") ||
-//             headers.get("authorization") != GUID
-//           ) {
-//             console.log("NO AUTHORIZATION");
-//             return Response.json({
-//               success: false,
-//               error: "Incorrect Authorization",
-//             });
-//           }
-//           const hook = new WebhookClient({
-//             url: Bun.env.DISCORD_TRADE_LOG_WEBHOOK_URL!,
-//           });
-//           const body = await req.json();
-//           const msg = body.msg;
-//           let regex = /Account Age - ([\s\S]*?) Days/g;
-//           let find = msg.match(regex);
-//           let isAgeBelow100 = false;
-//           if (find) {
-//             find.forEach((element: any) => {
-//               let n = element
-//                 .replace("Account Age - ", "")
-//                 .replace(" Days", "")
-//                 .replace(",", "");
-//               n = parseFloat(n);
-//               if (isAgeBelow100 == false && n <= 100) {
-//                 isAgeBelow100 = true;
-//               }
-//             });
-//           }
-//           // if(msg.search("Dealing damage faster than a usual rate.") >= 0) return;
-//           const embed = new EmbedBuilder()
-//             .setTitle("Trade Log")
-//             .setFooter({ text: "from game" })
-//             .setDescription(msg);
+          const hook = new WebhookClient({
+            url: Bun.env.DISCORD_EXPLOIT_LOG_WEBHOOK_URL!,
+          });
+          const body = await req.json();
+          const msg = body.msg;
+          // let randomadmin = await this.discord.getRandomAdmin();
+          // randomadmin = randomadmin || "No Admin Is Online";
+          // if(msg.search("Dealing damage faster than a usual rate.") >= 0) return;
+          const embed = new EmbedBuilder()
+            .setTitle("Exploit Logged")
+            .setFooter({ text: "from game" })
+            .setDescription(msg);
+          // hook.send({content: `<@${randomadmin.user.id}>`, embeds: [embed]});
+          await hook.send({
+            content: `Temp removed admin ping`,
+            embeds: [embed],
+          });
+          return Response.json({ success: true });
+        } catch (er) {
+          return Response.json({ success: false, error: er });
+        }
+      },
+    },
+    "/trade/webhook/t": {
+      POST: async (req: Request) => {
+        try {
+          let headers = req.headers;
+          if (
+            !headers.get("authorization") ||
+            headers.get("authorization") != GUID
+          ) {
+            console.log("NO AUTHORIZATION");
+            return Response.json({
+              success: false,
+              error: "Incorrect Authorization",
+            });
+          }
+          const hook = new WebhookClient({
+            url: Bun.env.DISCORD_TRADE_LOG_WEBHOOK_URL!,
+          });
+          const body = await req.json();
+          const msg = body.msg;
+          let regex = /Account Age - ([\s\S]*?) Days/g;
+          let find = msg.match(regex);
+          let isAgeBelow100 = false;
+          if (find) {
+            find.forEach((element: any) => {
+              let n = element
+                .replace("Account Age - ", "")
+                .replace(" Days", "")
+                .replace(",", "");
+              n = parseFloat(n);
+              if (isAgeBelow100 == false && n <= 100) {
+                isAgeBelow100 = true;
+              }
+            });
+          }
+          // if(msg.search("Dealing damage faster than a usual rate.") >= 0) return;
+          const embed = new EmbedBuilder()
+            .setTitle("Trade Log")
+            .setFooter({ text: "from game" })
+            .setDescription(msg);
 
-//           if (isAgeBelow100) {
-//             embed.setColor("#FFCCCB");
-//           }
-//           await hook.send({ embeds: [embed] });
-//           return Response.json({ success: true });
-//         } catch (er) {
-//           return Response.json({ success: false, error: er });
-//         }
-//       },
-//     },
-//   },
+          if (isAgeBelow100) {
+            embed.setColor("#FFCCCB");
+          }
+          await hook.send({ embeds: [embed] });
+          return Response.json({ success: true });
+        } catch (er) {
+          return Response.json({ success: false, error: er });
+        }
+      },
+    },
+  },
 
-//   // (optional) fallback for unmatched routes:
-//   // Required if Bun's version < 1.2.3
-//   fetch(req) {
-//     const host = req.headers.get("host");
-//     // e.g. "api.example.com:3000"
+  // (optional) fallback for unmatched routes:
+  // Required if Bun's version < 1.2.3
+  fetch(req) {
+    const host = req.headers.get("host");
+    // e.g. "api.example.com:3000"
 
-//     const hostname = host?.split(":")[0]; // remove port
-//     const subdomain = hostname?.split(".")[0];
+    const hostname = host?.split(":")[0]; // remove port
+    const subdomain = hostname?.split(".")[0];
 
-//     if (subdomain === "api" && req.url.endsWith("/bansv4")) {
-//       if (BANS) {
-//         return Response.json(BANS);
-//       } else {
-//         return Response.json({ success: false });
-//       }
-//     }
+    if (subdomain === "api" && req.url.endsWith("/bansv4")) {
+      if (BANS) {
+        return Response.json(BANS);
+      } else {
+        return Response.json({ success: false });
+      }
+    }
 
-//     return new Response("Not Found", { status: 404 });
-//   },
-// });
+    return new Response("Not Found", { status: 404 });
+  },
+});
 
 // console.log(`Server running at ${server.url}`);
 // client.on(Events.ShardError, (error) => {
@@ -235,9 +235,9 @@ client.modals = new Collection();
 // // When the client is ready, run this code (only once).
 // // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // // It makes some properties non-nullable.
-// client.once(Events.ClientReady, (readyClient: ClientType<true>) => {
-//   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-// });
+client.once(Events.ClientReady, (readyClient: ClientType<true>) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
 // client.on(Events.Error, (error) => {
 //   console.error("The client encountered an error:", error);
 // });
